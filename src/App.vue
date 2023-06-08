b26d0a638de6dfcb285cbde82275cbfdb1c78982
Viktor Persson <viktor.persson@arcsin.se>
2023-06-08 13:14:06 +0200

    Add links to discord, hub and wiki.

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,12 @@ import Events from "./components/Events.vue";
   <div class="container max-w-6xl">
     <div class="flex justify-between my-4">
       <div class="py-2">
-        <h1 class="text-4xl mb-0.5">
+        <h1 class="text-4xl mb-1">
           Quake<span class="text-sky-400">.</span>World
         </h1>
         <div class="text-sky-200">a non-profit supporting QuakeWorld</div>
       </div>
       <div class="flex gap-4 m-4 items-center">
-        <div>
-          <a
-            class="bg-sky-300/10 hover:bg-sky-300/20 rounded-xl p-1 flex items-center border border-sky-300/10 opacity-70 hover:opacity-100"
-            href="http://discord.quake.world/"
-            title="Quake.World Discord"
-          >
-            <img class="w-8 h-8" src="/assets/img/logos/discord.svg" />
-            <span class="hidden sm:inline ml-0.5 pr-1.5">Discord</span>
-          </a>
-        </div>
         <div>
           <a
             class="bg-sky-300/10 hover:bg-sky-300/20 rounded-xl p-1 flex items-center border border-sky-300/10 opacity-70 hover:opacity-100"
@@ -38,19 +28,68 @@ import Events from "./components/Events.vue";
       </div>
     </div>
 
-    <hr class="mt-0 mb-8" />
+    <hr class="mt-0 mb-6" />
 
-    <!--    <div-->
-    <!--      class="my-6 p-6 text-green-100 border border-l-8 border-green-400/20 bg-green-600/10 text-green-200"-->
-    <!--    >-->
-    <!--      <MediaObject-->
-    <!--        color="text-green-400"-->
-    <!--        description="Supporting critical parts of the QuakeWorld community such as-->
-    <!--          software, servers, websites, competitions and events."-->
-    <!--        icon="goal"-->
-    <!--        title="Our Mission"-->
-    <!--      />-->
-    <!--    </div>-->
+    <div class="lg:grid mb-6 grid-cols-3 gap-6">
+      <a
+        class="flex items-center p-4 justify-between border border-sky-300/10 bg-sky-600/10 hover:bg-sky-600/20 opacity-70 hover:opacity-100"
+        href="http://discord.quake.world/"
+      >
+        <div class="flex items-center">
+          <img class="h-10 mr-4" src="/assets/img/logos/discord.svg" />
+          <div>
+            <div class="app-font-slab">Discord</div>
+            <div class="text-sm text-gray-300">Join 4000+ fellow Quakers</div>
+          </div>
+        </div>
+
+        <img
+          class="w-5 h-5 opacity-50"
+          src="/assets/img/icons/arrow_forward_ios.svg"
+        />
+      </a>
+
+      <a
+        class="flex items-center p-4 justify-between border border-sky-300/10 bg-sky-600/10 hover:bg-sky-600/20 opacity-70 hover:opacity-100"
+        href="http://hub.quakeworld.nu/"
+      >
+        <div class="flex items-center">
+          <img
+            class="h-5 lg:h-10 mr-4"
+            src="/assets/img/logos/quakeworld_hub.png"
+          />
+          <div>
+            <div class="app-font-slab">Hub</div>
+            <div class="text-sm text-gray-300">
+              Live games, streams and events
+            </div>
+          </div>
+        </div>
+
+        <img
+          class="w-5 h-5 opacity-50"
+          src="/assets/img/icons/arrow_forward_ios.svg"
+        />
+      </a>
+
+      <a
+        class="flex items-center p-4 justify-between border border-sky-300/10 bg-sky-600/10 hover:bg-sky-600/20 opacity-70 hover:opacity-100"
+        href="http://wiki.quakeworld.nu/"
+      >
+        <div class="flex items-center">
+          <img class="h-10 mr-4" src="/assets/img/logos/wiki.png" />
+          <div>
+            <div class="app-font-slab">Wiki</div>
+            <div class="text-sm text-gray-300">Everything QuakeWorld</div>
+          </div>
+        </div>
+
+        <img
+          class="w-5 h-5 opacity-50"
+          src="/assets/img/icons/arrow_forward_ios.svg"
+        />
+      </a>
+    </div>
 
     <div class="md:grid grid-cols-2 gap-6">
       <div
