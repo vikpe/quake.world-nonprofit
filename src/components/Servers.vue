706a6f399beb0e8561483e9946f563eb4064b16c
Viktor Persson <viktor.persson@arcsin.se>
2023-09-01 15:25:41 +0200

    Add city to server list.

--- src/components/Servers.vue
+++ src/components/Servers.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import { ref } from "vue";
-import { Server } from "./../types.ts";
+import {ref} from "vue";
+import {Server} from "./../types.ts";
 import Flag from "./Flag.vue";
 import MediaObject from "./MediaObject.vue";
 
@@ -16,30 +16,33 @@ getData();
 
 <template>
   <div
-    class="mb-6 md:mb-0 px-8 py-6 border border-sky-600/30"
-    style="
+      class="mb-6 md:mb-0 px-8 py-6 border border-sky-600/30"
+      style="
       background: #040d21 url(/assets/img/globe.jpg) no-repeat right center;
       background-size: contain;
     "
   >
     <div class="text-blue-200">
       <MediaObject
-        color="text-blue-400"
-        description="High quality QuakeWorld servers"
-        title="Servers"
+          color="text-blue-400"
+          description="High quality QuakeWorld servers"
+          title="Servers"
       />
     </div>
     <div class="mt-4 text-left">
       <table>
         <tbody>
-          <tr v-for="server in servers">
-            <td class="p-1">
-              <Flag :cc="server.country.cc" />
-            </td>
-            <td class="p-1">
-              <code>{{ server.hostname }}</code>
-            </td>
-          </tr>
+        <tr v-for="server in servers">
+          <td class="p-1">
+            <Flag :cc="server.location.cc"/>
+          </td>
+          <td class="p-1">
+            <code>{{ server.hostname }}</code>
+          </td>
+          <td class="p-1 text-sm text-gray-400">
+            ({{ server.location.city }})
+          </td>
+        </tr>
         </tbody>
       </table>
     </div>
--- src/types.ts
+++ src/types.ts
@@ -1,37 +1,38 @@
 export interface OpenCollectiveUser {
-  memberID: number;
-  createdAt: string;
-  type: string;
-  role: "ADMIN" | "BACKER";
-  isActive: boolean;
-  totalAmountDonated: number;
-  currency: string;
-  lastTransactionAt: string;
-  lastTransactionAmount: number;
-  profile: string;
-  name: string;
-  company: string;
-  description: string;
-  image: string;
-  email: string;
-  twitter: string;
-  github: string;
-  website: string;
+    memberID: number;
+    createdAt: string;
+    type: string;
+    role: "ADMIN" | "BACKER";
+    isActive: boolean;
+    totalAmountDonated: number;
+    currency: string;
+    lastTransactionAt: string;
+    lastTransactionAmount: number;
+    profile: string;
+    name: string;
+    company: string;
+    description: string;
+    image: string;
+    email: string;
+    twitter: string;
+    github: string;
+    website: string;
 }
 
 export interface Server {
-  hostname: string;
-  country: User;
-  admin: User;
+    hostname: string;
+    location: Location;
+    admin: User;
 }
 
-export interface User {
-  name: string;
-  cc: string;
+export interface Location {
+    name: string;
+    cc: string;
+    city: string;
 }
 
 export interface Link {
-  title: string;
-  url: string;
-  description?: string;
+    title: string;
+    url: string;
+    description?: string;
 }
